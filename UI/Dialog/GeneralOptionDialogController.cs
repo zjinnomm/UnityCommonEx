bd1620ccb23705a71b0b46ef525a404619e1f8ca
zjinn <zjinn@aliyun.com>
2026-05-13 22:32:22 +0800

    GeneralOptionConfig 也仿照Popup的范式

--- UI/Dialog/GeneralOptionDialogController.cs
+++ UI/Dialog/GeneralOptionDialogController.cs
@@ -1,48 +1,75 @@
 using System;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 namespace UnityCommonEx
 {
+    public class GeneralOptionDialogButtonConfig
+    {
+        public string Title;
+        public string Subtitle;
+        public Sprite Icon;
+        public Action Callback;
+        public bool Interactable = true;
+    }
+
+    public struct GeneralOptionDialogConfig
+    {
+        public string Message;
+        public GeneralOptionDialogButtonConfig Button1;
+        public GeneralOptionDialogButtonConfig Button2;
+        public GeneralOptionDialogButtonConfig Button3;
+    }
 
     public class GeneralOptionDialogController : NodeController
     {
 
         public TMP_Text MessageText;
-        public Transform OptionButtonRoot;
-        public GameObject OptionButtonPrefab;
+        public RichButtonController Button1;
+        public RichButtonController Button2;
+        public RichButtonController Button3;
 
-        List<RichButtonController> optionButtons = new List<RichButtonController>();
-        List<Action> callbacks = new List<Action>();
+        private const int ButtonSlotCount = 3;
+        private readonly Action[] callbacks = new Action[ButtonSlotCount];
+        private RichButtonController[] buttons;
 
-        public struct Option
+        protected override void OnInit()
         {
-            public string Title;
-            public string Subtitle;
-            public Sprite Icon;
-            public Action Callback;
-            public bool Interactable;
+            base.OnInit();
+            buttons = new[] { Button1, Button2, Button3 };
+            Clear();
         }
 
-        public void Set(string message, IList<Option> options)
+        public void Set(in GeneralOptionDialogConfig config)
         {
             Clear();
-            MessageText.text = message;
-            for (int i = 0; i < options.Count; i++)
-            {
-                int index = i;
-                Option option = options[i];
-                RichButtonController button = Create<RichButtonController>(OptionButtonPrefab, OptionButtonRoot);
-                button.Set(option.Title, () => { OnButtonClick(index); }, option.Icon, option.Subtitle, option.Interactable);
-                optionButtons.Add(button);
-                callbacks.Add(option.Callback);
-            }
+            MessageText.text = config.Message;
+            ApplyButton(0, config.Button1);
+            ApplyButton(1, config.Button2);
+            ApplyButton(2, config.Button3);
+        }
+
+        void ApplyButton(int index, GeneralOptionDialogButtonConfig config)
+        {
+            if (buttons == null || index < 0 || index >= buttons.Length)
+                return;
+
+            RichButtonController button = buttons[index];
+            if (button == null)
+                return;
+
+            bool visible = config != null && !string.IsNullOrEmpty(config.Title);
+            button.gameObject.SetActive(visible);
+            callbacks[index] = visible ? config.Callback : null;
+            if (!visible)
+                return;
+
+            button.Set(config.Title, () => { OnButtonClick(index); }, config.Icon, config.Subtitle, config.Interactable);
         }
 
         void OnButtonClick(int index)
         {
-            if (index < callbacks.Count)
+            if (index >= 0 && index < callbacks.Length)
             {
                 callbacks[index]?.Invoke();
             }
@@ -52,15 +79,19 @@ namespace UnityCommonEx
         public void Clear()
         {
             MessageText.text = null;
-            foreach (var button in optionButtons)
+            if (buttons == null)
+                return;
+
+            foreach (var button in buttons)
             {
                 if (button != null)
                 {
-                    button.Destroy();
+                    button.gameObject.SetActive(false);
                 }
             }
-            optionButtons.Clear();
-            callbacks.Clear();
+
+            for (int i = 0; i < callbacks.Length; i++)
+                callbacks[i] = null;
         }
 
     }
